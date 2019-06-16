bbc6b0d81cc09a563c1d631a5a6a533b25845257
lindyer <895309655@qq.com>
2019-06-16 23:18:13 +0800

    add general net protocol

---
+++ Src/Network/NetProtocol.cpp
@@ -0,0 +1,75 @@
+﻿#include "NetProtocol.h"
+#include "Common/ConvertHelper.h"
+#include <winsock2.h>
+#include <QDebug>
+
+
+namespace neo {
+	
+	QString NetProtocol::Header::toString() const {
+		return QString("Version: %1,Command: %2,MsgSize: %3").arg(version).arg(cmd).arg(msgSize);
+	}
+
+
+	QByteArray NetProtocol::Message::serialize() const {
+		QByteArray ba;
+		ba.append(uint16ToByteArray(header.version));
+		ba.append(uint16ToByteArray(header.cmd));
+		ba.append(uint32ToByteArray(header.msgSize));
+		ba.append(body);
+		return ba;
+	}
+
+
+	QByteArray NetProtocol::pack(const QByteArray& body, quint16 cmd, quint16 version) {
+		QByteArray ba;
+		ba.append(uint16ToByteArray(version));
+		ba.append(uint16ToByteArray(cmd));
+		ba.append(uint32ToByteArray(ProtocolHeaderSize + quint32(body.size())));
+		ba.append(body);
+		return ba;
+	}
+
+
+	NetProtocol::Message NetProtocol::unpack(const QByteArray & data) {
+		Message msg;
+		msg.header = parseHeader(data.mid(0, ProtocolHeaderSize));
+		msg.body = data.mid(ProtocolHeaderSize);
+		return msg;
+	}
+
+
+	NetProtocol::Header NetProtocol::parseHeader(const QByteArray & header) {
+		Header h;
+		h.version = byteArrayToUint16(header.mid(0, 2));
+		h.cmd = byteArrayToUint16(header.mid(2, 2));
+		h.msgSize = byteArrayToUint32(header.mid(4, 4));
+		return h;
+	}
+
+
+	void NetProtocol::parseData(QByteArray & data, const std::function<void(const NetProtocol::Message&)> & msgHandler) {
+		int versionSize = sizeof(quint16);
+		if (data.size() >= versionSize) {
+			auto version = data.mid(0, versionSize);
+			if (uint16ToByteArray(NetProtocol::Version01) != version) {
+				qWarning() << "Version doesn't match,drop it";
+				data.clear();
+				return;
+			}
+		}
+		if (data.size() < NetProtocol::ProtocolHeaderSize) {
+			return;
+		}
+		auto header = parseHeader(data.mid(0, NetProtocol::ProtocolHeaderSize));
+		if (header.msgSize <= data.size()) {
+			auto item = data.mid(0, header.msgSize);  //取出一个完整的信息包
+			data.remove(0, header.msgSize);
+			auto msg = unpack(item);
+			if (msgHandler) {
+				msgHandler(msg);
+			}
+			return;
+		}
+	}
+}
---
+++ Src/Network/NetProtocol.h
@@ -0,0 +1,95 @@
+﻿#pragma once
+
+#include <functional>
+
+#include <QtGlobal>
+#include <QString>
+#include <QByteArray>
+
+namespace neo {
+	
+	class NetProtocol {
+	public:
+		enum Version {
+			Version01
+		};
+
+		enum Command {
+			MsgText = 1,
+			SevenZipFiles
+		};
+
+		struct Header {
+			quint16 version = 0;               //版本号
+			quint16 cmd = 0;                   //命令码
+			quint32 msgSize = 0;               //消息体大小
+			QString toString() const;
+		};
+
+		constexpr static int ProtocolHeaderSize = sizeof(Header);
+
+		struct Message {
+			Header header;
+			QByteArray body;
+			QByteArray serialize() const;
+		};
+
+		QByteArray pack(const QByteArray& body, quint16 cmd = MsgText, quint16 version = Version01);
+
+		Message unpack(const QByteArray& data);
+
+		Header parseHeader(const QByteArray& header);
+
+		void parseData(QByteArray& data, const std::function<void(const Message&)>& msgHandler);
+
+	};
+
+	template<typename T>
+	struct ConnectionItem {
+		T* socket = nullptr;
+		QByteArray buffer;
+		~ConnectionItem() {
+			socket->close();
+			socket->deleteLater();
+			buffer.clear();
+		}
+	};
+
+
+	template<typename T>
+	struct ConnectionList : public QList<ConnectionItem<T>*> {
+		using QList<ConnectionItem<T>*>::size;
+		using QList<ConnectionItem<T>*>::at;
+		using QList<ConnectionItem<T>*>::removeAt;
+		ConnectionItem<T>* findConnection(T* socket) {
+			for (int i = 0; i < size(); i++) {
+				auto& item = at(i);
+				if (socket == item->socket) {
+					return item;
+				}
+			}
+			return nullptr;
+		}
+		bool remove(T* socket) {
+			for (int i = 0; i < size(); i++) {
+				auto& item = at(i);
+				if (socket == item->socket) {
+					removeAt(i);
+					return true;
+				}
+			}
+			return false;
+		}
+		void add(T* socket, QByteArray buffer = QByteArray()) {
+			auto conn = new ConnectionItem<T>();
+			conn->socket = socket;
+			conn->buffer = buffer;
+			append(conn);
+		}
+	};
+
+
+	
+}
+
+
